65627e281d50a122863f46d2336935d165a64236
Shane Pearman <spearman@github.com>
2018-05-06 22:51:58 -0700

    update apis documentation

--- apis/implementors/core/fmt/trait.Display.js
+++ apis/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["apis"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"apis/channel/struct.SendError.html\" title=\"struct apis::channel::SendError\">SendError</a>&lt;T&gt;",];
+implementors["apis"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"apis/channel/struct.SendError.html\" title=\"struct apis::channel::SendError\">SendError</a>&lt;T&gt;",synthetic:false,types:["apis::channel::SendError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- apis/implementors/std/error/trait.Error.js
+++ apis/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["apis"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"apis/channel/struct.SendError.html\" title=\"struct apis::channel::SendError\">SendError</a>&lt;T&gt;",];
+implementors["apis"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"apis/channel/struct.SendError.html\" title=\"struct apis::channel::SendError\">SendError</a>&lt;T&gt;",synthetic:false,types:["apis::channel::SendError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
